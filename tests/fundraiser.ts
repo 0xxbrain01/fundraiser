3eb3e93189675308552e53b6f5cae900b374b992
0xxbrain01 <trinhvanvinh018>
2024-09-08 19:21:32 +0700

    test

--- tests/fundraiser.ts
+++ tests/fundraiser.ts
@@ -7,10 +7,33 @@ describe("fundraiser", () => {
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.Fundraiser as Program<Fundraiser>;
+  const maker = anchor.web3.Keypair.generate();
+
+  let mint;
+  let contributorATA;
+  let makerATA;
+  let wallet;
+  const fundraiser = anchor.web3.PublicKey.findProgramAddressSync(
+    [
+      Buffer.from('fundraiser_seed'),
+     maker.publicKey.toBuffer() 
+    ],
+    program.programId
+  )[0];
+  const contributor = anchor.web3.PublicKey.findProgramAddressSync(
+    [
+      Buffer.from('contributer_seed'),
+     fundraiser.toBuffer(),
+     anchor.AnchorProvider.env().publicKey.toBuffer()
+    ],
+    program.programId
+  )[0];
+
+  console.log("🚀 ~ describe ~ fundraiser:", fundraiser, contributor);
 
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    // const tx = await program.methods.initialize().rpc();
+    // console.log("Your transaction signature", tx);
   });
 });
